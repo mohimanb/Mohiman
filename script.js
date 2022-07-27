a5ea9ab19cb50d94d27bda82646360a9b1a56523
Mohiman Bhuiya <61213934+mohimanb@users.noreply.github.com>
2022-07-27 15:31:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -46,22 +46,3 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
-
-
-//FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
-function autoPlayYouTubeModal(){
-  var trigger = $("body").find('[data-toggle="modal"]');
-  trigger.click(function() {
-    var theModal = $(this).data( "target" ),
-    videoSRC = $(this).attr( "data-theVideo" ), 
-    videoSRCauto = videoSRC+"?autoplay=1" ;
-    $(theModal+' iframe').attr('src', videoSRCauto);
-    $(theModal+' button.close').click(function () {
-        $(theModal+' iframe').attr('src', videoSRC);
-    });   
-  });
-}
-
-$(document).ready(function(){
-  autoPlayYouTubeModal();
-});
